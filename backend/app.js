bff1bd61fbe49a950cef234ad7995f7253a22bb2
Jaimie Mathangi <jaimie.mathangi78@gmail.com>
2026-06-24 17:57:18 +0200

    wire middleware into app.js

--- backend/app.js
+++ backend/app.js
@@ -5,18 +5,23 @@ import reviewRouter  from './routes/review.js'
 import healthRouter  from './routes/health.js'
 import historyRouter from './routes/history.js'
 
+import { requestLogger } from './middleware/logger.js'
+import reviewLimiter from './middleware/rateLimit.js'
+import { validateReviewBody } from './middleware/validate.js'
+import { errorHandler } from './middleware/errors.js'
+
 const app = express()
 
 app.use(express.json())
 app.use(cors(corsOptions))
+app.use(requestLogger)
+
+app.use('/api/review', reviewLimiter, validateReviewBody)
 
 app.use('/api', reviewRouter)
 app.use('/api', healthRouter)
 app.use('/api', historyRouter)
 
-app.use((err, req, res, next) => {
-  console.error(err.stack)
-  res.status(500).json({ error: 'Internal server error' })
-})
+app.use(errorHandler)
 
 export default app
